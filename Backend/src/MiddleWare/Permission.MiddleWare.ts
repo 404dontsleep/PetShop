d13a8ddba7784b9c7770a779c224da1483cce1ff
404DontSleep <404dontsleep@gmail.com>
2024-11-16 16:45:12 +0700

    Complete Permission MiddleWare

---
+++ Backend/src/MiddleWare/Permission.MiddleWare.ts
@@ -0,0 +1,13 @@
+import { TRole } from "@MyTypes/User.Type";
+import { NextFunction, Request, Response } from "express";
+import { IAuthRequest } from "./Auth.MiddleWare";
+
+export const PermissionMiddleWare = (permissions: TRole[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    if (!permissions.includes((req as IAuthRequest).User.Role)) {
+      res
+        .status(403)
+        .json({ message: "Forbidden", error: "Permission denied" });
+    } else next();
+  };
+};
